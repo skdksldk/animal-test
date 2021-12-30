375fd14ae1710f845f7c0ef44960ab74bb870469
skdksldk <tnwns1485@naver.com>
2021-12-30 11:05:45 +0900

    animal share

---
+++ js/share.js
@@ -0,0 +1,18 @@
+const shareButton = document.querySelector(".share_btn");
+
+
+function copyUrl() {
+    let url = window.location.href;
+    let tmp = document.createElement('input');
+    
+
+    document.body.appendChild(tmp);
+    tmp.value = url;
+    tmp.select();
+	document.execCommand("copy");
+    document.body.removeChild(tmp);
+    
+	alert("URL이 복사되었습니다"); 
+}
+
+shareButton.addEventListener('click', copyUrl);
